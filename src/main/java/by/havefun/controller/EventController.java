d78a23f82bc91f61b17438a9095932b5a8bdf93b
brillliant <brillliant@tut.by>
2014-11-11 12:17:41 -0300

    #15 4 Прикрутил tiles

--- src/main/java/by/havefun/controller/EventController.java
+++ src/main/java/by/havefun/controller/EventController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import by.havefun.entity.Event;
 
@@ -32,6 +33,12 @@ public class EventController extends AbstractController {
         model.addAttribute("events", eventDao.getEventsAfter(LocalDateTime.now()));
         setRequiedName(model, principal, "Main Page");
         return "events";
+    }
+    
+    //temp
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+    public ModelAndView getTestPage() {
+		return new ModelAndView("test");
     }
 
     /**
