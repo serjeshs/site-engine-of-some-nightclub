de01df25d79766ea5c6b58c5dbb1aaff13b85e68
Andrei Ladyka <andrei@ladyka.tk>
2017-12-11 20:18:37 +0300

    add menu and footer components

--- republic/src/main/angular/src/app/app-routing.module.ts
+++ republic/src/main/angular/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {MenuComponent} from "./menu/menu.component";
+import {EventsComponent} from "./events/events.component";
 
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroesComponent} from './heroes/heroes.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+// import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'detail/:id', component: HeroDetailComponent},
-  {path: 'heroes', component: HeroesComponent}
+  {path: '', redirectTo: '/afisha', pathMatch: 'full'},
+  {path: 'afisha', component: EventsComponent},
+  {path: 'menu', component: MenuComponent},
+  // {path: 'detail/:id', component: HeroDetailComponent},
 ];
 
 @NgModule({
--- republic/src/main/angular/src/app/app.module.ts
+++ republic/src/main/angular/src/app/app.module.ts
@@ -6,15 +6,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-import {HeroesComponent} from './heroes/heroes.component';
-import {HeroSearchComponent} from './hero-search/hero-search.component';
-import {HeroService} from './hero.service';
-import {MessageService} from './message.service';
-import {MessagesComponent} from './messages/messages.component';
 import { EventsComponent } from './events/events.component';
 import { EventsService } from './events.service';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
@@ -25,14 +20,11 @@ import { EventsService } from './events.service';
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent,
-    EventsComponent
+    EventsComponent,
+    MenuComponent,
+    FooterComponent,
   ],
-  providers: [HeroService, MessageService, EventsService],
+  providers: [EventsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
