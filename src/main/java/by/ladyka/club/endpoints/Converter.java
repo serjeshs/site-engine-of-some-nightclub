3dcff98c33d74a88a8dc87cccdfe42dcbbba076a
Andrei Ladyka <andrei@ladyka.tk>
2018-04-24 17:45:44 +0300

    write log to file and update spring-boot version.

--- src/main/java/by/ladyka/club/endpoints/Converter.java
+++ src/main/java/by/ladyka/club/endpoints/Converter.java
@@ -1,6 +1,8 @@
 package by.ladyka.club.endpoints;
 
 import by.ladyka.club.service.ConverterService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,12 +14,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Converter {
 
-    @Autowired
-    ConverterService converterService;
+	private final ConverterService converterService;
+	private static final Logger logger = LogManager.getLogger(Converter.class);
 
-    @RequestMapping(value = "api/converter", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    ResponseEntity f() {
-        return new ResponseEntity<>(converterService.convertDataBase(), HttpStatus.OK);
-    }
+	@Autowired
+	public Converter(ConverterService converterService) {
+		this.converterService = converterService;
+	}
+
+	@RequestMapping(value = "api/converter", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody
+	ResponseEntity f() {
+		return new ResponseEntity<>(converterService.convertDataBase(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "api/logger", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody
+	ResponseEntity logger() {
+		logger.trace("TRACE_TEST_TRACE");
+		logger.debug("LOG " + System.currentTimeMillis());
+		logger.info("IMAGINATION " + System.currentTimeMillis());
+		logger.warn("OLOLO");
+		logger.error("О БОЖЕ ТЫ МОЙ!!!");
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
+		return new ResponseEntity<>(true, HttpStatus.OK);
+	}
 }
