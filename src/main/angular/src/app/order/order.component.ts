9b67038aaa38fcd0216f111e33873443fc165ad0
Andrei Ladyka <andrei@ladyka.tk>
2018-09-29 23:15:46 +0300

    #36 load menu in order view by link

--- src/main/angular/src/app/order/order.component.ts
+++ src/main/angular/src/app/order/order.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {MenuOrder} from "../dto/menuOrder";
 import {MenuService} from "../menu.service";
-import {EventsService} from "../events.service";
 import {MenuCategoryDto} from "../dto/menuCategoryDto";
 
 @Component({
@@ -15,15 +14,18 @@ export class OrderComponent implements OnInit {
   order: MenuOrder;
   menuCategories: MenuCategoryDto[];
 
-  constructor(private activatedRoute: ActivatedRoute, private menuService: MenuService, private eventsService: EventsService) {
-    this.menuCategories = []
-    this.order = new MenuOrder()
-    this.order.foodPrice = {}
-    this.order.food = {}
+  constructor(private activatedRoute: ActivatedRoute, private menuService: MenuService) {
+    this.menuCategories = [];
+    this.order = new MenuOrder();
+    this.order.foodPrice = {};
+    this.order.food = {};
   }
 
   ngOnInit() {
     this.uuid = this.activatedRoute.snapshot.paramMap.get('uuid');
+    this.menuService.getMenu().subscribe(c => {
+      this.menuCategories = c;
+    });
     this.menuService.getOrderByUuid(this.uuid)
       .subscribe(order => {
           if (order.id) {
