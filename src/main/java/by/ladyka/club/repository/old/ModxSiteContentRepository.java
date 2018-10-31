e2213ba3ac798b555f756a59f5dd08853a3a363a
Andrei Ladyka <andrei@ladyka.tk>
2018-10-31 10:35:59 +0300

    Remove modx code

--- src/main/java/by/ladyka/club/repository/old/ModxSiteContentRepository.java
+++
@@ -1,9 +0,0 @@
-package by.ladyka.club.repository.old;
-
-import by.ladyka.club.entity.old.ModxSiteContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModxSiteContentRepository extends JpaRepository<ModxSiteContent, Long> {
-}
