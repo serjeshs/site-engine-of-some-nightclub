c75058d154c17f0994cbb65375524974af62f58c
Andrei Ladyka <andrei@ladyka.tk>
2018-11-13 21:23:07 +0300

    Update top banner.

--- src/main/angular/src/app/dto/settingsBanner.ts
+++ src/main/angular/src/app/dto/settingsBanner.ts
@@ -1,6 +1,4 @@
 export class SettingsBanner {
   public eventId : number;
   public imageUrl : string;
-  public eventName : string;
-  public startTime : string;
 }
--- src/main/angular/src/app/top-banner/top-banner.component.ts
+++ src/main/angular/src/app/top-banner/top-banner.component.ts
@@ -14,9 +14,7 @@ export class TopBannerComponent implements OnInit {
   ngOnInit() {
     this.settingsBanner = new SettingsBanner();
     this.settingsBanner.eventId = 1113;
-    this.settingsBanner.eventName = 'HАРРУ NEW YEAR';
     this.settingsBanner.imageUrl = 'https://republic-club.by/files/2018/11/banner.jpg';
-    this.settingsBanner.startTime = '31 декабря 2018 г., 22:00'
   }
 
 }
