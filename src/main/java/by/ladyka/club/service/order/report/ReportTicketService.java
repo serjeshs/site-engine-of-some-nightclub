ea132a49b65d848da48f5f037dc73d980e8d560a
Andrei Ladyka <andrei@ladyka.tk>
2019-01-25 20:46:26 +0300

    #39 improvement order.pdf

--- src/main/java/by/ladyka/club/service/order/report/ReportTicketService.java
+++ src/main/java/by/ladyka/club/service/order/report/ReportTicketService.java
@@ -1,6 +1,7 @@
 package by.ladyka.club.service.order.report;
 
 import by.ladyka.club.dto.report.TicketOrderReportDto;
+import by.ladyka.club.dto.tikets.TablePlaceDto;
 import by.ladyka.club.service.order.OrderTicketsService;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.design.JRDesignStyle;
@@ -31,7 +32,22 @@ public class ReportTicketService {
 
 		Map<String, Object> parameters = new HashMap<>();
 		parameters.put("title", "Билет № " + order.getId());
+		order.setPayStatus(translatePayStatus(order.getPayStatus()));
 		parameters.put("order", order);
+
+		//TODO rewrite this part of report  !!!!11111 FFFUUUUUCCCCKKKKKK
+
+		if (order.getTables().size() > 0) {
+			StringBuilder tablePlaces = new StringBuilder();
+			for (TablePlaceDto tablePlaceDto : order.getTables()) {
+				tablePlaces.append(String.format("Столик №%d. Место №%d\n", tablePlaceDto.getTable(), tablePlaceDto.getPlace()));
+			}
+			parameters.put("tablePlaces", tablePlaces.toString());
+		} else {
+			parameters.put("tablePlaces", "Нет");
+		}
+
+
 
 		final JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 		JRDesignStyle defaultStyle = new JRDesignStyle();
@@ -43,5 +59,9 @@ public class ReportTicketService {
 
 		jasperPrint.setDefaultStyle(defaultStyle);
 		simpleExporter.exportToPdf(jasperPrint, outputStream);
+	}
+
+	private String translatePayStatus(String payStatus) {
+		return "successful".equals(payStatus) ? "Оплачено" : "Не оплачено";
 	}
 }
