81bb77ec5c32cac72773df507fa9ac688e311c4f
Serjeshs <serjeshs@gmail.com>
2019-02-14 01:21:20 +0300

    test with all context

--- src/main/java/by/ladyka/club/service/PictureServiceImpl.java
+++ src/main/java/by/ladyka/club/service/PictureServiceImpl.java
@@ -4,30 +4,34 @@ import by.ladyka.club.entity.EventEntity;
 import by.ladyka.club.repository.EventRepository;
 import by.ladyka.club.service.files.StorageService;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+@Service
+@Transactional
 @AllArgsConstructor
 public class PictureServiceImpl implements PictureService {
     private final EventRepository eventRepository;
     private final StorageService storageService;
+
+    private static final Logger logger = LogManager.getLogger(PictureServiceImpl.class);
 
     @Override
     public void fetchCovers() throws IOException {
         List<EventEntity> events = eventRepository.findAll();
         for (EventEntity event : events) {
             String coverUri = event.getCoverUri();
-//            Pattern pattern = Pattern.compile("http");
-//            Matcher matcher = pattern.matcher(event.getCoverUri());
-//            if (matcher.find()){
-//            }
             if (coverUri.matches("^http.+")) {
                 try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
                     HttpGet httpget = new HttpGet(coverUri);
@@ -35,26 +39,15 @@ public class PictureServiceImpl implements PictureService {
                     int statusCode = response.getStatusLine().getStatusCode();
                     if (statusCode == 200) {
                         InputStream content = response.getEntity().getContent();
-//                      for test
-                        BufferedReader rd = new BufferedReader(
-                                new InputStreamReader(response.getEntity().getContent()));
-
-                        StringBuffer result = new StringBuffer();
-                        String line = "";
-                        while ((line = rd.readLine()) != null) {
-                            result.append(line);
-                        }
-                        System.out.println(result.toString());
-//                      end test
-
-//                        String newName = storageService.store("test", content);
-//                        event.setCoverUri(newName);
-//                        eventRepository.save(event);
+                        String newName = storageService.store(FilenameUtils.getName(coverUri), content);
+                        event.setCoverUri(newName);
+                        eventRepository.save(event);
+                        response.close();
+                    } else {
+                        logger.error("remote server returned an error");
                     }
                 }
 
-            } else {
-                System.out.println("не надо загружать");
             }
 
         }
--- src/test/java/by/ladyka/club/service/PictureServiceImplTest.java
+++ src/test/java/by/ladyka/club/service/PictureServiceImplTest.java
@@ -1,28 +1,38 @@
 package by.ladyka.club.service;
 
+import by.ladyka.club.config.CustomSettings;
 import by.ladyka.club.entity.EventEntity;
 import by.ladyka.club.repository.EventRepository;
 import by.ladyka.club.service.files.StorageService;
-import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-
+@DataJpaTest
+@Transactional
 @RunWith(SpringRunner.class)
+//@ComponentScan({"by.ladyka.club.service.files", "by.ladyka.club.config"})
+@ComponentScan({"by.ladyka.club.service.files"})
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class PictureServiceImplTest {
 
-    @MockBean
+//    @MockBean
+    @Autowired
     private EventRepository eventRepository;
-    @MockBean
+
+//    @MockBean
+    @Autowired
     private StorageService storageService;
 
     private PictureService pictureService;
@@ -35,8 +45,9 @@ public class PictureServiceImplTest {
         eventEntity = new EventEntity();
         eventEntity.setCoverUri("https://pp.userapi.com/c841137/v841137810/7580a/l2RQOIh2G9E.jpg");
 //        eventEntity.setCoverUri("userapi.com/c841137/v841137810/7580a/l2RQOIh2G9E.jpg");
-        when(eventRepository.findAll()).thenReturn(Collections.singletonList(eventEntity));
-//        when(storageService.store())
+        eventRepository.save(eventEntity);
+//        when(eventRepository.findAll()).thenReturn(Collections.singletonList(eventEntity));
+//        when(storageService.store()).thenReturn()
 
     }
 
@@ -44,6 +55,11 @@ public class PictureServiceImplTest {
     public void fetchCovers() {
         try {
             pictureService.fetchCovers();
+            List<EventEntity> eventList = eventRepository.findAll();
+            for (EventEntity event : eventList
+                 ) {
+                System.out.println(event.getCoverUri());
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
